9ce19bf6e1f1bae472bd66d7737e6985c659aa75
connorjnel <connor.j.nel00@gmail.com>
2023-05-13 22:30:28 +0100

    data types

--- beginner/complete-intro-to-web-development-v3/js/scratch.js
+++ beginner/complete-intro-to-web-development-v3/js/scratch.js
@@ -1,6 +1,10 @@
-// Output to frontend
-const result = [];
-const restultsContainer = document.getElementById('results');
+// Output to frontend - Basics
+const resultOne = [];
+const restultsContainerOne = document.getElementById('resultsOne');
+
+// Output to frontend - Number, String, Boolean
+const resultTwo = [];
+const restultsContainerTwo = document.getElementById('resultsTwo');
 
 function outputResults(item) {
 	let resultItem = document.createElement('li');
@@ -12,14 +16,40 @@ function outputResults(item) {
 const monthlyRent = 500;
 const yearlyRent = monthlyRent * 12;
 
-result.push(`Simple calculation = ` + yearlyRent);
+resultOne.push(`Simple calculation = ` + yearlyRent);
 
 let mango = 'tasty';
-result.push(`Changing a let variable = ` + mango);
+resultOne.push(`Changing a let variable = ` + mango);
 mango = 'sour';
-result.push(`Changing a let variable = ` + mango);
+resultOne.push(`Changing a let variable = ` + mango);
 
 // Output results by iterating the result array
-result.forEach((item) => {
-	restultsContainer.appendChild(outputResults(item));
+resultOne.forEach((item) => {
+	restultsContainerOne.appendChild(outputResults(item));
+});
+
+// Numbers, Strings, Booleans
+let firstName = 'Connor';
+resultTwo.push(`String = ` + firstName);
+
+let lastName = 'Nel';
+resultTwo.push(`String Concat = ` + firstName + ' ' + lastName);
+
+let myString3 = 'Hello' + ' ' + 'World';
+resultTwo.push(`String Concat Backquote = ${firstName} ${lastName}`);
+
+let skyIsBlue = true;
+resultTwo.push(`Is Sky Blue? = ` + skyIsBlue);
+
+let pigsCanFly = false;
+resultTwo.push(`Can pigs fly? = ` + pigsCanFly);
+
+let number = 51223458533;
+resultTwo.push(`Number integer = ` + number);
+
+let numberTwo = 3.1415;
+resultTwo.push(`Number float = ` + numberTwo);
+
+resultTwo.forEach((item) => {
+	restultsContainerTwo.appendChild(outputResults(item));
 });
