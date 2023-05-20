660cc297de6300e48cff3a7e7c3b623968f5ddd5
connorjnel <connor.j.nel00@gmail.com>
2023-05-20 23:04:59 +0100

    context

--- beginner/complete-intro-to-web-development-v3/js/scratch.js
+++ beginner/complete-intro-to-web-development-v3/js/scratch.js
@@ -247,3 +247,15 @@ ${this.location.country}`;
 };
 
 console.log(me.getAddress());
+
+// Arrays
+
+const daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(daysOfTheWeek);
+console.log(daysOfTheWeek[0]);
+console.log(daysOfTheWeek[1]);
+console.log(daysOfTheWeek[6]);
+
+const primeNumbers = [1, 2, 3, 5, 7, 11, 13, 17];
+console.log(primeNumbers.length);
+console.log(primeNumbers.join(' | '));
