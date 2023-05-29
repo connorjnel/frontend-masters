a3bb2aa9a2f17c3bb6713ed94cb5b058b953ee1d
connorjnel <connor.j.nel00@gmail.com>
2023-05-29 20:47:36 +0100

    calling api

--- beginner/complete-intro-to-web-development-v3/talking-to-server/scratch.js
+++ beginner/complete-intro-to-web-development-v3/talking-to-server/scratch.js
@@ -1,6 +1,7 @@
-fetch("./scratch.json")
-	.then((response) => response.json())
-	.then((json) => console.log(json, "- First"));
+// Impoting JSON from file, dont use module import, so many issues
+// fetch("./scratch.json")
+// 	.then((response) => response.json())
+// 	.then((json) => console.log(json, "- First"));
 
 // pretend this came from a server instead of me just declaring it here.
 const responseFromServer = `{"name": "Luna","age": 10,"breed": "Havanese","location": {"city":"Seattle","state": "WA"}}`;
@@ -12,4 +13,59 @@ const responseObject = JSON.parse(responseFromServer);
 console.log(responseObject.name); // just the name
 console.log(responseObject.location.city); // just the city
 console.log(responseObject); // the whole object
-console.log(responseObject.cute); // the whole object
+console.log(responseObject.cute);
+
+const dog = {
+	name: "Luna",
+	age: 10,
+	breed: "Havanese",
+	location: {
+		city: "Seattle",
+		state: "WA",
+	},
+};
+
+const objString = JSON.stringify(dog);
+console.log(objString);
+
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const doggos = document.getElementById("dog-target");
+
+function addNewDoggo() {
+	const promise = fetch(DOG_URL);
+	promise
+		.then(function (response) {
+			const processingPromise = response.text();
+			return processingPromise;
+		})
+		.then(function (processedResponse) {
+			const dogObject = JSON.parse(processedResponse);
+			const img = document.createElement("img");
+			img.src = dogObject.message;
+			img.alt = "Cute doggo";
+			doggos.appendChild(img);
+		});
+}
+
+document.getElementById("dog-btn").addEventListener("click", addNewDoggo);
+
+async function addDoggo() {
+	let url = "https://dog.ceo/api/breeds/image/random";
+	try {
+		let response = await fetch(url);
+		return await response.json();
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function trial() {
+	let data = await addDoggo();
+	console.log(data.message);
+	document.body.style.backgroundImage = `url("${data.message}")`;
+	document.body.style.backgroundRepeat = "no-repeat";
+	document.body.style.backgroundSize = "cover";
+}
+
+trial();
