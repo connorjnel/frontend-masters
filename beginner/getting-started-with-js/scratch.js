34d4dae7dd2c4db6352d188f94b749e576e5ecfe
connorjnel <connor.j.nel00@gmail.com>
2023-06-29 00:07:37 +0100

    primer exercise

--- beginner/getting-started-with-js/scratch.js
+++ beginner/getting-started-with-js/scratch.js
@@ -1,20 +1,29 @@
 // Loops
 
-let colors = ["red", "blue", "green"];
+// let colors = ["red", "blue", "green"];
 
-// Classic for loop
-for (let i = 0; i < colors.length; i++) {
-	console.log(colors[i]);
+// // Classic for loop
+// for (let i = 0; i < colors.length; i++) {
+// 	console.log(colors[i]);
+// }
+
+// // For of loop, variable of iterable, kinda like python
+// for (let color of colors) {
+// 	console.log(color);
+// }
+
+// // While loop, no initial value, no increment,
+// // this one runs in reverse idx order because of pop
+// while (colors.length > 0) {
+// 	let color = colors.shift();
+// 	console.log(color);
+// }
+
+// Functions
+// A piece of code you can run multiple times, can add parameters as input if defined
+
+function hello(name = "Test") {
+	console.log(`Hello, ${name}`);
 }
 
-// For of loop, variable of iterable, kinda like python
-for (let color of colors) {
-	console.log(color);
-}
-
-// While loop, no initial value, no increment,
-// this one runs in reverse idx order
-while (colors.length > 0) {
-	let color = colors.pop();
-	console.log(color);
-}
+hello("Jovan");
